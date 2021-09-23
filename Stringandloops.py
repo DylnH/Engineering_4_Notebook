03ca67aafb3e615b316c49a712a71c46f10f2545
Dylan H <57632909+DylnH@users.noreply.github.com>
2021-09-23 09:50:16 -0400

    Update Stringandloops.py

--- Stringandloops.py
+++ Stringandloops.py
@@ -3,8 +3,8 @@ import numpy
 txt = input("Write somethin' ")
 
 letters = list(txt)
-array1 = numpy.array(letters)
+array1 = numpy.array(letters) # formats letters vertically
 
 for i in letters:
-    newStr = i.replace(' ', '-') 
+    newStr = i.replace(' ', '-')  # instead of space, it a "-"
     print(newStr)
