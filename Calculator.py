3a6ffb9ca10edac4aac957eb123b2e71048d51cd
Dylan H <57632909+DylnH@users.noreply.github.com>
2021-09-28 10:39:08 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,38 +2,26 @@
 
 # Written by [Dylan J. Hensley]
 
-def add(x, y): # adds two numbers
- return x + y
-def subtract(x, y): # subtracts two numbers
- return x - y
-def multiply(x, y): # multiply two numbers
- return x * y
-def divide(x, y): # divide two numbers
- return x / y
-
-
-print("Select operation.") # Shows options/what you want to calculate
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+def doMath(x,y,z):
+    if(z==1):#addition
+        return x + y
+    if(z==2):#subtraction
+         return x - y
+    if(z==3):#mulitplication
+         return x * y
+    if(z==4):#division
+         return round(x / y, 2)
+    if(z==5):#modulo
+         return x % y
 
 while True:
- 
- choice = input("Enter choice(A/S/M/D): ") # what you write to Add/Subtract/Multiply/Divide
 
- if choice in ('A', 'S', 'M', 'D'): # check if choice is one of the four options
-     num1 = float(input("Enter first number: "))
-     num2 = float(input("Enter second number: "))
+        x = float(input("Enter 1st number: "))
+        y = float(input("Enter 2nd number: "))
 
-     if choice == 'A':
-         print(num1, "+", num2, "=", add(num1, num2)) # setup for Adding
-
-     elif choice == 'S':
-         print(num1, "-", num2, "=", subtract(num1, num2)) # setup for subtracting
-
-     elif choice == 'M':
-         print(num1, "*", num2, "=", multiply(num1, num2)) # setup for multiplying
-
-     elif choice == 'D':
-         print(num1, "/", num2, "=", divide(num1, num2)) # setup for Dividing 
+        print("Sum:        ", doMath(x,y,1))
+        print("Difference: ", doMath(x,y,2))
+        print("Product:    ", doMath(x,y,3))
+        print("Quotient:   ", doMath(x,y,4))
+        print("Modulo:     ", doMath(x,y,5))
+        break 
