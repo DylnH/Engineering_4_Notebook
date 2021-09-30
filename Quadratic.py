5a8cbcf18740086eb8f43fa3aa6e1e6656868c73
Dylan H <57632909+DylnH@users.noreply.github.com>
2021-09-30 09:34:32 -0400

    Update Quadratic.py

--- Quadratic.py
+++ Quadratic.py
@@ -1,19 +1,33 @@
-# Quadratic Solver
+#Quadratic Solver
 
-# Written by Dylan J. Hensley 
 
-import math
-a=float(input("Enter the coefficient a ")) # type in coefficient a
-b=float(input("Enter the coefficient b ")) # type in coefficient b
-c=float(input("Enter the coefficient c ")) # type in coefficient c
-d=b*b-4*a*c;
+#Written by: Dylan J. Hensley
 
-if d>0:
- r1 = (-b + math.sqrt(d)) / (2 * a) # show answer
- r2 = (-b - math.sqrt(d)) / (2 * a)
- print("Roots are real and unequal ",r1, " and ",r2) # if roots are real and unequal
-elif d==0:
- r1=-b/(2*a)
- print("Roots are real and equal ",r1) # if roots are real
-else:
- print("No real roots ") # if there's no roots
+def quadCalc(a,b,c): 
+  intA = int(a) 
+  intB = int(b) 
+  intC = int(c) 
+  disc = ((intB*intB)-(4*intA*intC)) 
+  Q1 = (-intB / (2*intA))
+  if disc < 0: 
+    return("no real roots.".format(intA, intB, intC))
+  if disc == 0:
+    return("The root =: {0}".format(Q1)) 
+  if disc > 0: 
+    pos = ((disc**0.5)/(2*intA)) 
+    w = round((Q1 - pos),5) 
+    x = round((Q1 + pos),5) 
+    return([w,x]) 
+
+while True:
+    print("Enter coefficients") #Asking for user input
+    a = input("Enter 1st coefficient: ") #input of 1st number
+    b = input("Enter 2nd coefficient: ") #input second number
+    c = input("Enter 3rd coefficient: ") #input third number
+    returnVal = quadCalc(a,b,c)
+    if isinstance(returnVal,list): 
+      print("Two roots:")
+      for root in returnVal:
+        print(root)
+    else:
+      print(returnVal) 
